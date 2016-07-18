8042a8d18038bdeedc504a4600a15a512613443c
Aksem <aders1234@gmail.com>
2016-07-18 21:31:20 +0300

    Added first version of new break screen.

--- koffeebreak/ui/break_screen.py
+++ koffeebreak/ui/break_screen.py
@@ -1,12 +1,17 @@
-from PyQt5.QtWidgets import QDialog
-#from ui.forms import break_screen_form
-from forms import break_screen_form
+from PyQt5.QtWidgets import QDialog, QWidget
+from PyQt5.QtCore import Qt
+from ui.forms import break_screen_form
+#from forms import break_screen_form
 
-class BreakWindow(QDialog):
+class BreakWindow(QWidget):
     def __init__(self):
         super(BreakWindow, self).__init__()
-        self.ui = break_screen_form.Ui_Form()
+        self.ui = break_screen_form.Ui_mainWidget()
         self.ui.setupUi(self)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setParent(None)
+        self.setAttribute(Qt.WA_NoSystemBackground)
+        self.setAttribute(Qt.WA_TranslucentBackground)
         self.showFullScreen()
 
 if __name__ == "__main__":
