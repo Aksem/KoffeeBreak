29aa7f00b3b596c83fb489ab4caa44c7a8729f84
Aksem <aders1234@gmail.com>
2016-09-11 15:44:00 +0200

    Added new settings & API for reading settings

--- koffeebreak/settings.py
+++ koffeebreak/settings.py
@@ -8,12 +8,25 @@ def read():
         settings['TIME'][time_item] = str(int(settings['TIME'][time_item]) * 60)
     return settings
 
+def read_parameter(config, parameter, p_type="str"):
+    if p_type == "str":
+        p_value = config[parameter[0]][parameter[1]]
+    elif p_type == "int":
+        p_value = int(config[parameter[0]][parameter[1]])
+    elif p_type == "bool":
+        p_value = bool(config[parameter[0]][parameter[1]])
+    else: 
+        p_value = None
+    return p_value
+
 def set_default(settings=None):
     #try to remove?
     settings['TIME'] = {'short_break': 5,
                         'long_break': 10,
-                        'short_work': 25,
+                        'work_time': 25,
                         'shorts_before_long': 3}
+    settings['EXECUTION'] = {'gui': 'qt',
+                             'state': 'work-full'}
     write(settings)
     
 def update(new_settings):
