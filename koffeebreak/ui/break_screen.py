fb61fe75d361e5c06594d413159eb2e6b68671ba
fablissimo <fabirovskij@gmail.com>
2016-09-20 12:42:36 +0300

    Added postponeBreak function, fixed some bugs & added some fetures.

--- koffeebreak/ui/break_screen.py
+++ koffeebreak/ui/break_screen.py
@@ -1,7 +1,8 @@
 from subprocess import Popen, PIPE
 
-from PyQt5.QtWidgets import QDialog, QWidget
+from PyQt5.QtWidgets import QDialog, QWidget, QVBoxLayout
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 from ui.forms import break_screen_form
 
 class BreakWindow(QWidget):
@@ -11,19 +12,29 @@ class BreakWindow(QWidget):
         self.ui.setupUi(self)
         #self.setWindowFlags(Qt.FramelessWindowHint)
         #self.setParent(None)
-        #self.setAttribute(Qt.WA_NoSystemBackground)
-        #self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setAttribute(Qt.WA_NoSystemBackground)
+        self.setAttribute(Qt.WA_TranslucentBackground)
         self.showFullScreen()
         self.gui_connection = gui_connection
-        
+        #self.gui_connection.changeState.connect(self.changeState)
+        self.gui_connection.whatTime.connect(self.setTime)
+
         self.ui.lockScreen_pushButton.clicked.connect(self.lockScreen)
         self.ui.breakComp_pushButton.clicked.connect(self.close)
         self.ui.skipBreak_pushButton.clicked.connect(self.skipBreak)
-    
+        self.ui.postponeBreak_pushButton.clicked.connect(self.postponeBreak)
+
+    def setTime(self, time):
+        self.ui.leftTime_label.setText('%02d:%02d' % (divmod(time, 60)))
+
     def skipBreak(self):
         self.gui_connection.skipBreak.emit()
         self.close()
-        
+
+    def postponeBreak(self):
+        self.gui_connection.postponeBreak.emit()
+        self.close()
+
     def lockScreen(self):
         # Popen with stdout for logs in future
         with Popen(['qdbus', 'org.freedesktop.ScreenSaver', '/ScreenSaver',
