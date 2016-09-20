cc13e61bcd7e4e8495f9687af2cdb39fab8b3f6d
Aksem <aders1234@gmail.com>
2016-09-20 13:02:48 +0200

    Revert "Settings dialog & break screen"

--- koffeebreak/ui/qt_gui_connection.py
+++ koffeebreak/ui/qt_gui_connection.py
@@ -2,9 +2,8 @@ from PyQt5.QtCore import pyqtSignal, QObject
 
 class qSignal(QObject):
     changeState = pyqtSignal(object)
-    whatTime = pyqtSignal(object)
+    whatTime = pyqtSignal()
+    timeIs = pyqtSignal(object)
     closeApp = pyqtSignal()
     skipBreak = pyqtSignal()
     pauseTimer = pyqtSignal()
-    postponeBreak = pyqtSignal()
-    startBreak = pyqtSignal()
