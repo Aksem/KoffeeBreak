e8a42517e0d4c3ef8e5c07401c6fe06aa110c0ed
fablissimo <fabirovskij@gmail.com>
2016-09-21 19:39:53 +0300

    Added transparent background to break screen

--- koffeebreak/ui/break_screen.py
+++ koffeebreak/ui/break_screen.py
@@ -10,11 +10,13 @@ class BreakWindow(QWidget):
         super(BreakWindow, self).__init__()
         self.ui = break_screen_form.Ui_mainWidget()
         self.ui.setupUi(self)
+
         #self.setWindowFlags(Qt.FramelessWindowHint)
         #self.setParent(None)
         self.setAttribute(Qt.WA_NoSystemBackground)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.showFullScreen()
+
         self.gui_connection = gui_connection
         #self.gui_connection.changeState.connect(self.changeState)
         self.gui_connection.whatTime.connect(self.setTime)
